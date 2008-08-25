33ad64a25f1a45bd355257d265d88541363c342c
alee <alee@aeb25c08-0953-0410-bd6c-f302445d6ae0>
2008-08-25 01:33:56 +0000

    Added a minor comment.
    git-svn-id: http://svn.appkido.com/svn/appkido/trunk@65 aeb25c08-0953-0410-bd6c-f302445d6ae0

--- src/GlobalClasses/AKFrameworkConstants.m
+++ src/GlobalClasses/AKFrameworkConstants.m
@@ -32,7 +32,7 @@ NSArray *_AKNamesOfEssentialFrameworks()
                 AKCoreDataFrameworkName,
                 AKCoreImageFrameworkName,
                 AKQuartzCoreFrameworkName,
-                @"ApplicationServices",
+                @"ApplicationServices",  // [agl] KLUDGE -- to get CGPoint etc.
                 nil] retain];
     }
 
