3d4acfecf2d537c2ca489038f81e99015d20b368
alee <alee@aeb25c08-0953-0410-bd6c-f302445d6ae0>
2008-08-25 13:14:25 +0000

    A comment.
    git-svn-id: http://svn.appkido.com/svn/appkido/trunk@83 aeb25c08-0953-0410-bd6c-f302445d6ae0

--- src/SingletonControllers/AKAppController.m
+++ src/SingletonControllers/AKAppController.m
@@ -215,6 +215,9 @@ static NSTimeInterval g_checkpointTime = 0.0;
 #if APPKIDO_FOR_IPHONE
     _appDatabase = [[AKDatabase databaseForPlatform:AKIPhonePlatform] retain];
 
+    // I'm going to assume anyone using AppKiDo for iPhone is going to want
+    // all the frameworks in the iPhone SDK.  There are so few of them it
+    // doesn't make sense to leave any out.
     [AKPrefUtils
         setSelectedFrameworkNamesPref:
             [[AKDocSetIndex
