de7fcfefe44b48de8fe0e9b69f94280d5e22b073
Andy Lee <aglee@mac.com>
2013-02-28 08:41:15 -0500

    Touched up the positions and titles of various buttons and text fields. Things look nicer and align a little nicer.

---
+++ src/CustomViews/AKButtonCell.h
@@ -0,0 +1,16 @@
+//
+//  AKButtonCell.h
+//  AppKiDo
+//
+//  Created by Andy Lee on 2/28/13.
+//  Copyright (c) 2013 Andy Lee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+/*!
+ * When enabled, draws the title in black. When disabled, draws in a slightly
+ * lighter gray than Cocoa tries to enforce.
+ */
+@interface AKButtonCell : NSButtonCell
+@end
---
+++ src/CustomViews/AKButtonCell.m
@@ -0,0 +1,34 @@
+//
+//  AKButtonCell.m
+//  AppKiDo
+//
+//  Created by Andy Lee on 2/28/13.
+//  Copyright (c) 2013 Andy Lee. All rights reserved.
+//
+
+#import "AKButtonCell.h"
+
+@implementation AKButtonCell
+
+// Technique learned from http://stackoverflow.com/questions/6370500/nsbutton-set-text-color-in-disabled-mode/10632311#10632311 for drawing the title in the color of my choice when a button is disabled. It looks like Apple passes an all-gray attributed string to this method.
+- (NSRect)drawTitle:(NSAttributedString *)title withFrame:(NSRect)frame inView:(NSView *)controlView
+{
+    // Hack the color of the title.
+    NSMutableParagraphStyle *style = [[[NSMutableParagraphStyle alloc] init] autorelease];
+    [style setAlignment:NSCenterTextAlignment];
+
+    NSColor *textColor = ([self isEnabled]
+                          ? [NSColor blackColor] /*[NSColor blueColor]*/
+                          : [NSColor colorWithCalibratedWhite:0.6 alpha:1.0] /*[NSColor yellowColor]*/);
+    NSDictionary *attr = (@{
+                          NSFontAttributeName: [self font],
+                          NSForegroundColorAttributeName: textColor,
+                          NSParagraphStyleAttributeName: style,
+                          });
+
+    title = [[[NSAttributedString alloc] initWithString:[title string]
+                                             attributes:attr] autorelease];
+    return [super drawTitle:title withFrame:frame inView:controlView];
+}
+
+@end
