17c0d040cc3ad1a5300d379c0ae4aa3a1c74b0a7
Andy Lee <aglee@mac.com>
2013-03-06 19:29:18 -0500

    Updated notes based on what I've been doing.

---
+++ src/GlobalClasses/AKPrefDictionary.h
@@ -0,0 +1,13 @@
+//
+//  AKPrefDictionary.h
+//  AppKiDo
+//
+//  Created by Andy Lee on 3/6/13.
+//  Copyright (c) 2013 Andy Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol AKPrefDictionary <NSObject>
+
+@end
