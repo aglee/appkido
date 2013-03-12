97696130470db3bd7ac9360eb4617079022ca0d8
Andy Lee <aglee@mac.com>
2013-03-11 20:52:19 -0400

    Decided the keyboard intercepting that AKTableView was doing didn't make sense, so removed it.

--- src/CustomViews/AKTableView.m
+++ src/CustomViews/AKTableView.m
@@ -28,59 +28,6 @@
 }
 
 #pragma mark -
-#pragma mark NSResponder methods
-
-// NSTableView doesn't trigger actions when you navigate with the keyboard.
-// This override does.
-- (void)keyDown:(NSEvent *)theEvent
-{
-    NSString *eventChars = [theEvent characters];
-
-    if ([eventChars isEqualToString:@"\n"]
-        || [eventChars isEqualToString:@"\r"])
-    {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-        [[self target] performSelector:[self doubleAction] withObject:self];
-#pragma clang diagnostic pop
-    }
-    else
-    {
-        NSInteger oldSelectedRow = [self selectedRow];
-
-        [super keyDown:theEvent];
-
-        if ([self selectedRow] != oldSelectedRow)
-        {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-            [[self target] performSelector:[self action] withObject:self];
-#pragma clang diagnostic pop
-        }
-    }
-}
-
-// Allow the user to use left and right arrow keys to move between the subtopic
-// list and the doc list.
-- (void)moveLeft:(id)sender
-{
-    if ([[self previousKeyView] isKindOfClass:[AKTableView class]])
-    {
-        [[self window] makeFirstResponder:[self previousKeyView]];
-    }
-}
-
-// Allow the user to use left and right arrow keys to move between the subtopic
-// list and the doc list.
-- (void)moveRight:(id)sender
-{
-    if ([[self nextKeyView] isKindOfClass:[AKTableView class]])
-    {
-        [[self window] makeFirstResponder:[self nextKeyView]];
-    }
-}
-
-#pragma mark -
 #pragma mark NSView methods
 
 - (BOOL)acceptsFirstMouse:(NSEvent *)theEvent
