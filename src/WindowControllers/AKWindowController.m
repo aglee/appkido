90a2c7b89aab9994a9832e0b16bf4effbd0c0c72
Andy Lee <aglee@mac.com>
2013-03-12 19:22:27 -0400

    Added logic to patch toolbar items into the tab chain if Full Keyboard Access is turned on. The logic is based on reverse-engineering the view hierarchy of the toolbar.

--- src/WindowControllers/AKWindowController.m
+++ src/WindowControllers/AKWindowController.m
@@ -233,6 +233,11 @@ static NSString *_AKToolbarID = @"AKToolbarID";
     AKWindow *w = (AKWindow *)[self window];
     NSMutableArray *tabChain = [NSMutableArray array];
 
+    if ([NSApp isFullKeyboardAccessEnabled] && [[w toolbar] isVisible])
+    {
+        [self _addToolbarItemsToTabChain:tabChain];
+    }
+    
     [tabChain addObject:[_topicBrowserController topicBrowser]];
 
     if ([NSApp isFullKeyboardAccessEnabled])
@@ -248,31 +253,117 @@ static NSString *_AKToolbarID = @"AKToolbarID";
 
     if ([_quicklistDrawer state] == NSDrawerOpenState)
     {
-        if ([NSApp isFullKeyboardAccessEnabled])
-        {
-            [tabChain addObject:[_quicklistController quicklistRadio1]];
-            [tabChain addObject:[_quicklistController quicklistRadio2]];
-            [tabChain addObject:[_quicklistController frameworkPopup]];
-            [tabChain addObject:[_quicklistController quicklistRadio3]];
-        }
-
-        [tabChain addObject:[_quicklistController searchField]];
-
-        if ([NSApp isFullKeyboardAccessEnabled])
-        {
-            [tabChain addObject:[_quicklistController searchOptionsPopup]];
-        }
-
-        [tabChain addObject:[_quicklistController quicklistTable]];
-
-        if ([NSApp isFullKeyboardAccessEnabled])
-        {
-            [tabChain addObject:[_quicklistController removeFavoriteButton]];
-        }
+        [self _addDrawerControlsToTabChain:tabChain];
     }
 
     [w removeAllTabChains];
     [w addLoopingTabChain:tabChain];
+}
+
+
+//NSView
+//	NSToolbarItemViewer
+//	NSControl
+//		_NSToolbarItemViewerLabelView
+//		NSButton
+//			NSToolbarButton
+//
+//
+//BEGIN nextKeyView sequence:
+//  <NSToolbarButton: 0xd175990>
+//  <NSToolbarItemViewer: 0x3967a90>
+//  <_NSToolbarItemViewerLabelView: 0x3967c20>
+//  <NSToolbarButton: 0xd1763f0>
+//  <NSToolbarItemViewer: 0x39681a0>
+//  <_NSToolbarItemViewerLabelView: 0x3968350>
+//  <NSToolbarButton: 0x39659d0>
+//  <NSToolbarItemViewer: 0x3968770>
+//  <_NSToolbarItemViewerLabelView: 0x3968920>
+//  <NSToolbarButton: 0x3965e00>
+//  <NSToolbarView: 0x236c720>
+//  <NSToolbarItemViewer: 0x3966210>
+//  <_NSToolbarItemViewerLabelView: 0x3966510>
+//  <NSToolbarButton: 0x237d870>
+//  <NSToolbarItemViewer: 0x39673e0>
+//  <_NSToolbarItemViewerLabelView: 0x39676b0>
+//  <NSToolbarButton: 0xd175990>
+//END nextKeyView sequence -- sequence contains a loop
+//
+//
+//NSThemeFrame
+//	<_NSThemeCloseWidget: 0xd1746c0>,
+//	<_NSThemeWidget: 0xd172dc0>,
+//	<_NSThemeWidget: 0xd1762a0>,
+//
+//	contentView <NSView: 0xd1b0d70>,
+//
+//	(<NSToolbarView: 0x236c720>: AKToolbarID)
+//		<_NSToolbarViewClipView: 0x232cab0>
+//			<NSToolbarItemViewer: 0x3966210 'AKQuicklistToolID'>,
+//				<_NSToolbarItemViewerLabelView: 0x3966510>,
+//				<NSToolbarButton: 0x237d870>
+//			<NSToolbarItemViewer: 0x39673e0 'AKBrowserToolID'>,
+//			<NSToolbarItemViewer: 0x3967a90 'AKBackToolID'>,
+//			<NSToolbarItemViewer: 0x39681a0 'AKForwardToolID'>,
+//			<NSToolbarItemViewer: 0x3968770 'AKSuperclassToolID'>
+
+
+- (void)_addToolbarItemsToTabChain:(NSMutableArray *)tabChain
+{
+    NSView *themeFrame = [[[self window] contentView] superview];
+    NSView *toolbarView = [[self _subviewsOf:themeFrame
+                               withClassName:@"NSToolbarView"] lastObject];
+    NSView *toolbarClipView = [[toolbarView subviews] lastObject];
+
+    for (NSView *toolbarItemViewer in [toolbarClipView subviews])
+    {
+        NSButton *toolbarButton = [[self _subviewsOf:toolbarItemViewer
+                                       withClassName:@"NSToolbarButton"] lastObject];
+        if ([toolbarButton isEnabled])
+        {
+            [tabChain addObject:toolbarButton];
+        }
+    }
+}
+
+- (NSArray *)_subviewsOf:(NSView *)view withClassName:(NSString *)viewClassName
+{
+    NSMutableArray *result = [NSMutableArray array];
+
+    for (NSView *subview in [view subviews])
+    {
+        if ([[subview className] isEqualToString:viewClassName])
+        {
+            [result addObject:subview];
+        }
+    }
+
+    return result;
+}
+
+- (void)_addDrawerControlsToTabChain:(NSMutableArray *)tabChain
+{
+    if ([NSApp isFullKeyboardAccessEnabled])
+    {
+        [tabChain addObject:[_quicklistController quicklistRadio1]];
+        [tabChain addObject:[_quicklistController quicklistRadio2]];
+        [tabChain addObject:[_quicklistController frameworkPopup]];
+        [tabChain addObject:[_quicklistController quicklistRadio3]];
+    }
+
+    [tabChain addObject:[_quicklistController searchField]];
+
+    if ([NSApp isFullKeyboardAccessEnabled])
+    {
+        [tabChain addObject:[_quicklistController searchOptionsPopup]];
+    }
+
+    [tabChain addObject:[_quicklistController quicklistTable]];
+
+    if ([NSApp isFullKeyboardAccessEnabled])
+    {
+        [tabChain addObject:[_quicklistController removeFavoriteButton]];
+    }
 }
 
 #pragma mark -
