93b80292825327add3c3b7b993e07d29da153097
Andy Lee <aglee@mac.com>
2013-03-14 14:41:33 -0400

    Added properties for the enabled and disabled text colors.

--- src/CustomViews/AKButtonCell.h
+++ src/CustomViews/AKButtonCell.h
@@ -9,8 +9,33 @@
 #import <Cocoa/Cocoa.h>
 
 /*!
- * When enabled, draws the title in black. When disabled, draws in a slightly
- * lighter gray than Cocoa tries to enforce.
+ * Allows you to specify the color of the button's title text, both when the
+ * button is enabled and when it's disabled.
+ *
+ * The implementation uses the technique learned from
+ * <http://stackoverflow.com/questions/6370500/nsbutton-set-text-color-in-disabled-mode/10632311#10632311>.
+ *
+ * I created this class because with a very small button (it just has a triangle
+ * "character" as its title) I find it a little hard to distinguish the enabled
+ * and disabled states.
+ *
+ * HOW TO USE
+ * ----------
+ * In IB, select a button's cell and change the cell's class to AKButtonCell.
+ * You can leave it that, in which case you will get this class's default text
+ * colors, or you can specify your own text colors.
  */
 @interface AKButtonCell : NSButtonCell
+{
+@private
+    NSColor *_enabledTextColor;
+    NSColor *_disabledTextColor;
+}
+
+/*! Defaults to a very dark gray (not quite black). */
+@property (nonatomic, copy) NSColor *enabledTextColor;
+
+/*! Defaults to a slightly lighter gray than Cocoa uses by default. */
+@property (nonatomic, copy) NSColor *disabledTextColor;
+
 @end
--- src/CustomViews/AKButtonCell.m
+++ src/CustomViews/AKButtonCell.m
@@ -10,16 +10,98 @@
 
 @implementation AKButtonCell
 
-// Technique learned from http://stackoverflow.com/questions/6370500/nsbutton-set-text-color-in-disabled-mode/10632311#10632311 for drawing the title in the color of my choice when a button is disabled. It looks like Apple passes an all-gray attributed string to this method.
+@synthesize enabledTextColor = _enabledTextColor;
+@synthesize disabledTextColor = _disabledTextColor;
+
+#define AKButtonCellEnabledTextColor [NSColor colorWithCalibratedWhite:0.2 alpha:1.0]
+#define AKButtonCellDisabledTextColor [NSColor colorWithCalibratedWhite:0.6 alpha:1.0]
+
+#pragma mark -
+#pragma mark NSCell methods
+
+- (void)_AKButtonCell_common_init
+{
+    _enabledTextColor = [AKButtonCellEnabledTextColor retain];
+    _disabledTextColor = [AKButtonCellDisabledTextColor retain];
+}
+
+- (id)initImageCell:(NSImage *)anImage
+{
+    self = [super initImageCell:anImage];
+    if (self)
+    {
+        [self _AKButtonCell_common_init];
+    }
+
+    return self;
+}
+
+- (id)initTextCell:(NSString *)aString
+{
+    self = [super initTextCell:aString];
+    if (self)
+    {
+        [self _AKButtonCell_common_init];
+    }
+
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self)
+    {
+        [self _AKButtonCell_common_init];
+    }
+
+    return self;
+}
+
+- (void)dealloc
+{
+    [_enabledTextColor release];
+    [_disabledTextColor release];
+
+    [super dealloc];
+}
+
+#pragma mark -
+#pragma mark Getters and setters
+
+- (void)setEnabledTextColor:(NSColor *)enabledTextColor
+{
+    [enabledTextColor retain];
+    [_enabledTextColor release];
+    _enabledTextColor = enabledTextColor;
+
+    [[self controlView] setNeedsDisplay:YES];
+}
+
+- (void)setDisabledTextColor:(NSColor *)disabledTextColor
+{
+    [disabledTextColor retain];
+    [_disabledTextColor release];
+    _disabledTextColor = disabledTextColor;
+
+    [[self controlView] setNeedsDisplay:YES];
+}
+
+#pragma mark -
+#pragma mark NSButtonCell methods
+
+// 
+// for drawing the title in the color of my choice when a button is disabled.
+// Apple's default behavior is to pass an all-gray version of the cell's title
+// to this method. This override replaces the gray with a different color.
 - (NSRect)drawTitle:(NSAttributedString *)title withFrame:(NSRect)frame inView:(NSView *)controlView
 {
-    // Hack the color of the title.
     NSMutableParagraphStyle *style = [[[NSMutableParagraphStyle alloc] init] autorelease];
-    [style setAlignment:NSCenterTextAlignment];
+    [style setAlignment:[self alignment]];
 
     NSColor *textColor = ([self isEnabled]
-                          ? [NSColor colorWithCalibratedWhite:0.2 alpha:1.0]
-                          : [NSColor colorWithCalibratedWhite:0.6 alpha:1.0]);
+                          ? [self enabledTextColor]
+                          : [self disabledTextColor]);
     NSDictionary *attr = (@{
                           NSFontAttributeName: [self font],
                           NSForegroundColorAttributeName: textColor,
