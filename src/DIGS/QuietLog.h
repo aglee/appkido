7a48f15494487ed41e921393cc623778c34c5d6a
Andy Lee <aglee@mac.com>
2013-04-04 17:03:33 -0400

    Added QuietLog(), thanks to Mark Dalrymple.

---
+++ src/DIGS/QuietLog.h
@@ -0,0 +1,14 @@
+//
+//  QuietLog.h
+//  AppKiDo
+//
+//  Created by Andy Lee on 4/4/13.
+//  Copyright (c) 2013 Andy Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/*!
+ * Credit: Mark Dalrymple <http://borkware.com/quickies/single?id=261>.
+ */
+extern void QuietLog (NSString *format, ...);
