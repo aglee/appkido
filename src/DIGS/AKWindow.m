094154805547a9f4b3b71cf17ebdf209879f84ec
Fritz Anderson <fritza@manoverboard.org>
2014-05-13 18:00:40 -0500

    Listen for cmd-L as an alias for sh-cmd-F (Search…)
    The usual IB voodoo from whenever it sees a XIB.

--- src/DIGS/AKWindow.m
+++ src/DIGS/AKWindow.m
@@ -23,4 +23,30 @@
 	[super setTitle:aString];
 }
 
+/** Intercept cmd-L as equivalent to sh-cmd-F
+ 
+ @author    Fritz Anderson <fritza@mac.com>
+ @date      13-May-2014
+ 
+ I keep hitting cmd-L as though the application had
+ a browser-style address/search bar. I see no harm
+ in honoring the usual key equivalent.
+ 
+ @bug   This method puts a hidden restriction on the use of cmd-L
+        for other purposes.
+ */
+- (void) keyDown:(NSEvent *)theEvent
+{
+    NSString *      chars = theEvent.characters;
+    NSUInteger      modifiers = theEvent.modifierFlags & NSDeviceIndependentModifierFlagsMask;
+    BOOL            onlyCmdKey = (modifiers == NSCommandKeyMask);
+
+    if ([chars isEqualToString: @"l"] && onlyCmdKey) {
+        [NSApp sendAction: @selector(selectSearchField:) to: nil from: self];
+    }
+    else {
+        [super keyDown: theEvent];
+    }
+}
+
 @end
