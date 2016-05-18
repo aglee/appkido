1aad69e0bafd0787db8ebba4d3ce9e4af38be521
Andy Lee <aglee@mac.com>
2016-05-18 13:19:10 -0400

    Added AKResult so I can move away from NSError** in my method arguments, and so I can enforce that there is always an NSError when the result is failure.

---
+++ GlobalClasses/AKResult.h
@@ -0,0 +1,30 @@
+//
+//  AKResult.h
+//  AppKiDo
+//
+//  Created by Andy Lee on 5/18/16.
+//  Copyright © 2016 Andy Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//TODO: Consider using generics for the object property.
+
+/*!
+ * self.object is or self.error is non-nil, not both.  Both may be nil.
+ */
+@interface AKResult : NSObject
+
+@property (strong, readonly) id object;
+@property (strong, readonly) NSError *error;
+
+/*! obj may be nil.  The returned result */
++ (AKResult *)successResultWithObject:(id)obj;
+
+/*! error must not be nil. */
++ (AKResult *)failureResultWithError:(NSError *)error;
++ (AKResult *)failureResultWithErrorDomain:(NSString *)domain
+                                      code:(NSInteger)code
+                               description:(NSString *)desc;
+
+@end
