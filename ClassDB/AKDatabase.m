4aa2f7c9130867af25e9b33d57a4b0789073f3dc
Andy Lee <aglee@mac.com>
2016-05-20 16:20:14 -0400

    Temporary kludge. Hard-coded a fictitious framework for protocols where I haven't figured out yet how to determine their real framework.

--- ClassDB/AKDatabase.m
+++ ClassDB/AKDatabase.m
@@ -184,6 +184,12 @@
 		frameworkName = [self _tryToInferFrameworkNameFromDocPath:docPath];
 	}
 
+	//TODO: KLUDGE -- Hard-code a fictitious "Unknown Framework" for protocols
+	// where I haven't figured out yet how to determine their real framework.
+	if (frameworkName == nil && [tokenMO.tokenType.typeName isEqualToString:@"intf"]) {
+		frameworkName = @"<UNKNOWN FRAMEWORK>";
+	}
+
 	return frameworkName;
 }
 
