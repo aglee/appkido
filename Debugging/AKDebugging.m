68ecb54f1e931a438ec1d6f125218f355db12db3
Andy Lee <aglee@mac.com>
2016-05-20 22:07:44 -0400

    Changed only whitespace.

--- Debugging/AKDebugging.m
+++ Debugging/AKDebugging.m
@@ -7,12 +7,10 @@
 //
 
 #import "AKDebugging.h"
-
 #import "AKAppDelegate.h"
 #import "AKTabChain.h"
 #import "AKWindow.h"
 #import "AKWindowController.h"
-
 #import "NSObject+AppKiDo.h"
 
 @implementation AKDebugging
@@ -21,145 +19,125 @@
 
 + (AKDebugging *)sharedInstance
 {
-    static AKDebugging *s_sharedInstance = nil;
-
-    if (!s_sharedInstance)
-    {
-        s_sharedInstance = [[self alloc] init];
-    }
-
-    return s_sharedInstance;
+	static AKDebugging *s_sharedInstance = nil;
+	if (!s_sharedInstance) {
+		s_sharedInstance = [[self alloc] init];
+	}
+	return s_sharedInstance;
 }
 
 #pragma mark - Initial setup
 
 + (BOOL)userCanDebug
 {
-    return ([NSUserName() isEqualToString:@"alee"]
-            && [NSFullUserName() isEqualToString:@"Andy Lee"]);
+	return ([NSUserName() isEqualToString:@"alee"]
+			&& [NSFullUserName() isEqualToString:@"Andy Lee"]);
 }
 
 - (void)addDebugMenu
 {
-    // Add the "Debug" menu item to the menu bar.
-    NSMenu *mainMenu = NSApp.mainMenu;
-    NSMenuItem *debugMenuItem = [mainMenu addItemWithTitle:@"Debug"
-                                                    action:@selector(testParser:)
-                                             keyEquivalent:@""];
-    [debugMenuItem setEnabled:YES];
+	// Add the "Debug" menu item to the menu bar.
+	NSMenu *mainMenu = NSApp.mainMenu;
+	NSMenuItem *debugMenuItem = [mainMenu addItemWithTitle:@"Debug"
+													action:@selector(testParser:)
+											 keyEquivalent:@""];
+	[debugMenuItem setEnabled:YES];
 
-    // Create the submenu that will be under the "Debug" top-level menu item.
-    NSMenu *debugSubmenu = [[NSMenu alloc] initWithTitle:@"Debug"];
+	// Create the submenu that will be under the "Debug" top-level menu item.
+	NSMenu *debugSubmenu = [[NSMenu alloc] initWithTitle:@"Debug"];
 
-    [debugSubmenu setAutoenablesItems:NO];
+	[debugSubmenu setAutoenablesItems:NO];
 
-    [debugSubmenu addItemWithTitle:@"Print First Responder"
-                            action:@selector(printFirstResponder:)
-                     keyEquivalent:@"r"];
-    [debugSubmenu addItemWithTitle:@"Print Modified Tab Chain"
-                            action:@selector(printModifiedTabChain:)
-                     keyEquivalent:@"l"];
-    [debugSubmenu addItemWithTitle:@"Print Unmodified Tab Chain"
-                            action:@selector(printUnmodifiedTabChain:)
-                     keyEquivalent:@"L"];
-    [debugSubmenu addItemWithTitle:@"Print nextValidKeyView Loop"
-                            action:@selector(printValidKeyViewLoop:)
-                     keyEquivalent:@""];
-    [debugSubmenu addItemWithTitle:@"Print nextKeyView Loop"
-                            action:@selector(printEntireKeyViewLoop:)
-                     keyEquivalent:@""];
-    [debugSubmenu addItemWithTitle:@"Print Window Info"
-                            action:@selector(printFunWindowFacts:)
-                     keyEquivalent:@"i"];
-    [debugSubmenu addItemWithTitle:@"Open Parser Testing Window"
-                            action:@selector(testParser:)
-                     keyEquivalent:@""];
+	[debugSubmenu addItemWithTitle:@"Print First Responder"
+							action:@selector(printFirstResponder:)
+					 keyEquivalent:@"r"];
+	[debugSubmenu addItemWithTitle:@"Print Modified Tab Chain"
+							action:@selector(printModifiedTabChain:)
+					 keyEquivalent:@"l"];
+	[debugSubmenu addItemWithTitle:@"Print Unmodified Tab Chain"
+							action:@selector(printUnmodifiedTabChain:)
+					 keyEquivalent:@"L"];
+	[debugSubmenu addItemWithTitle:@"Print nextValidKeyView Loop"
+							action:@selector(printValidKeyViewLoop:)
+					 keyEquivalent:@""];
+	[debugSubmenu addItemWithTitle:@"Print nextKeyView Loop"
+							action:@selector(printEntireKeyViewLoop:)
+					 keyEquivalent:@""];
+	[debugSubmenu addItemWithTitle:@"Print Window Info"
+							action:@selector(printFunWindowFacts:)
+					 keyEquivalent:@"i"];
+	[debugSubmenu addItemWithTitle:@"Open Parser Testing Window"
+							action:@selector(testParser:)
+					 keyEquivalent:@""];
 
-    // Attach the submenu to the "Debug" top-level menu item.
-    [mainMenu setSubmenu:debugSubmenu forItem:debugMenuItem];
+	// Attach the submenu to the "Debug" top-level menu item.
+	[mainMenu setSubmenu:debugSubmenu forItem:debugMenuItem];
 }
 
 #pragma mark - Action methods
 
 - (IBAction)testParser:(id)sender
 {
-    QLog(@"%s is now a no-op", __PRETTY_FUNCTION__);
+	QLog(@"%s is now a no-op", __PRETTY_FUNCTION__);
 }
 
 - (IBAction)printFirstResponder:(id)sender
 {
-    id firstResponder = NSApp.keyWindow.firstResponder;
-
-    if (firstResponder == nil)
-    {
-        NSLog(@"The key window has no first responder.");
-    }
-    else
-    {
-        NSLog(@"The key window's first responder is <%@: %p>.", [firstResponder className], firstResponder);
-    }
+	id firstResponder = NSApp.keyWindow.firstResponder;
+	if (firstResponder == nil) {
+		NSLog(@"The key window has no first responder.");
+	} else {
+		NSLog(@"The key window's first responder is <%@: %p>.", [firstResponder className], firstResponder);
+	}
 }
 
 - (IBAction)printModifiedTabChain:(id)sender
 {
-    NSLog(@"MODIFIED TAB CHAIN for %@", [NSApp.keyWindow ak_bareDescription]);
-
-    for (NSView *v in [AKTabChain modifiedTabChainForWindow:NSApp.keyWindow])
-    {
-        NSLog(@"  %@", [v ak_bareDescription]);
-    }
-
-    NSLog(@"END MODIFIED TAB CHAIN for %@\n\n", [NSApp.keyWindow ak_bareDescription]);
+	NSLog(@"MODIFIED TAB CHAIN for %@", [NSApp.keyWindow ak_bareDescription]);
+	for (NSView *v in [AKTabChain modifiedTabChainForWindow:NSApp.keyWindow]) {
+		NSLog(@"  %@", [v ak_bareDescription]);
+	}
+	NSLog(@"END MODIFIED TAB CHAIN for %@\n\n", [NSApp.keyWindow ak_bareDescription]);
 }
 
 - (IBAction)printUnmodifiedTabChain:(id)sender
 {
-    NSLog(@"UNMODIFIED TAB CHAIN for %@", [NSApp.keyWindow ak_bareDescription]);
-
-    for (NSView *v in [AKTabChain unmodifiedTabChainForWindow:NSApp.keyWindow])
-    {
-        NSLog(@"  %@", [v ak_bareDescription]);
-    }
-
-    NSLog(@"END UNMODIFIED TAB CHAIN for %@\n\n", [NSApp.keyWindow ak_bareDescription]);
+	NSLog(@"UNMODIFIED TAB CHAIN for %@", [NSApp.keyWindow ak_bareDescription]);
+	for (NSView *v in [AKTabChain unmodifiedTabChainForWindow:NSApp.keyWindow]) {
+		NSLog(@"  %@", [v ak_bareDescription]);
+	}
+	NSLog(@"END UNMODIFIED TAB CHAIN for %@\n\n", [NSApp.keyWindow ak_bareDescription]);
 }
 
 - (IBAction)printValidKeyViewLoop:(id)sender
 {
-    [self _printViewSequenceWithKeyPath:@"nextValidKeyView"];
+	[self _printViewSequenceWithKeyPath:@"nextValidKeyView"];
 }
 
 - (IBAction)printEntireKeyViewLoop:(id)sender
 {
-    [self _printViewSequenceWithKeyPath:@"nextKeyView"];
+	[self _printViewSequenceWithKeyPath:@"nextKeyView"];
 }
 
 - (IBAction)printFunWindowFacts:(id)sender
 {
-    AKWindowController *wc = [[AKAppDelegate appDelegate] frontmostWindowController];
-
-    if (wc == nil)
-    {
-        NSLog(@"No AppKiDo window is open.");
-    }
-    else
-    {
-        [wc printFunFacts:sender];
-    }
+	AKWindowController *wc = [[AKAppDelegate appDelegate] frontmostWindowController];
+	if (wc == nil) {
+		NSLog(@"No AppKiDo window is open.");
+	} else {
+		[wc printFunFacts:sender];
+	}
 }
 
 #pragma mark - Private methods
 
 - (void)_printViewSequenceWithKeyPath:(NSString *)nextViewKeyPath
 {
-    [self printFirstResponder:nil];
-    
-    id firstResponder = NSApp.keyWindow.firstResponder;
-
-    if ([firstResponder isKindOfClass:[NSView class]])
-    {
-        [firstResponder ak_printSequenceWithKeyPath:nextViewKeyPath];
-    }
+	[self printFirstResponder:nil];
+	id firstResponder = NSApp.keyWindow.firstResponder;
+	if ([firstResponder isKindOfClass:[NSView class]]) {
+		[firstResponder ak_printSequenceWithKeyPath:nextViewKeyPath];
+	}
 }
 
 @end
