1220cafa6a270f56f111e3359c6d657990d4cac4
Andy Lee <aglee@mac.com>
2021-02-11 03:24:00 -0500

    Grabbed latest TCMXMLWriter, which fixes compiler warning about init.
    - See commit <https://github.com/monkeydom/TCMXMLWriter/commit/678db797d1573747adc1b7a65d81d850190f7828>.

--- AppKiDo/Utilities/Third-Party/TCMXMLWriter/TCMXMLWriter.h
+++ AppKiDo/Utilities/Third-Party/TCMXMLWriter/TCMXMLWriter.h
@@ -10,32 +10,26 @@
 
 #define TCMWriterOptionNoOptions = 0UL;
 
-typedef NS_OPTIONS(NSUInteger, TCMXMLWriterOptions) {
+enum {
 	TCMXMLWriterOptionPrettyPrinted = 1UL << 0,
-	TCMXMLWriterOptionOrderedAttributes = 1UL << 1 // mainly for unit testing to get reproducible results, but can also be helpful otherwise for stable results
+	TCMXMLWriterOptionOrderedAttributes = 1UL << 1, // mainly for unit testing to get reproducible results, but can also be helpful otherwise for stable results
+	TCMXMLWriterOptionPrettyBOOL = 1UL << 2 // prints BOOLEAN NSNumbers as boolYESValue and boolNOValue values - default is "yes" and "no" 
 };
+typedef NSUInteger TCMXMLWriterOptions;
 
-@interface TCMXMLWriter : NSObject
-{
-@private
-    NSURL *_fileURL;
-    NSDateFormatter *_dateFormatter;
-    BOOL _currentTagHasContent;
-    NSMutableString *_indentationString;
-    TCMXMLWriterOptions _writerOptions;
-    NSOutputStream *_outputStream;
-    NSMutableArray *_elementNameStackArray;
-}
+@interface TCMXMLWriter : NSObject {}
 
 @property (nonatomic,strong) NSURL *fileURL;
-@property (weak, readonly) NSString *XMLString; // works for in memory streams and file URLs
-@property (weak, readonly) NSData *XMLData; // works for in memory streams and file URLs
+@property (unsafe_unretained, readonly) NSString *XMLString; // works for in memory streams and file URLs
+@property (unsafe_unretained, readonly) NSData *XMLData; // works for in memory streams and file URLs
 @property (nonatomic, strong) NSDateFormatter *dateFormatter;
+@property (nonatomic, strong) NSString *boolYESValue;
+@property (nonatomic, strong) NSString *boolNOValue;
 
-- (instancetype)initWithOptions:(TCMXMLWriterOptions)anOptionField NS_DESIGNATED_INITIALIZER; // stores marshalled stuff in string
-- (instancetype)initWithOptions:(TCMXMLWriterOptions)anOptionField fileURL:(NSURL *)aFileURL;
+- (id)initWithOptions:(TCMXMLWriterOptions)anOptionField; // stores marshalled stuff in string
+- (id)initWithOptions:(TCMXMLWriterOptions)anOptionField fileURL:(NSURL *)aFileURL;
 // stream must be open
-- (instancetype)initWithOptions:(TCMXMLWriterOptions)anOptionField outputStream:(NSOutputStream *)anOutputStream;
+- (id)initWithOptions:(TCMXMLWriterOptions)anOptionField outputStream:(NSOutputStream *)anOutputStream;
 
 // meta
 - (void)instructXMLStandalone; // <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
@@ -45,6 +39,7 @@ typedef NS_OPTIONS(NSUInteger, TCMXMLWriterOptions) {
 
 // asynchronous tags
 - (void)openTag:(NSString *)aTagName attributes:(NSDictionary *)anAttributeDictionary;
+- (void)openTag:(NSString *)aTagName; // attributeless
 - (void)closeLastTag;
 - (void)tag:(NSString *)aTagName attributes:(NSDictionary *)anAttributeDictionary; // contentless
 
@@ -55,6 +50,13 @@ typedef NS_OPTIONS(NSUInteger, TCMXMLWriterOptions) {
 - (void)tag:(NSString *)aTagName attributes:(NSDictionary *)anAttributeDictionary contentXML:(NSString *)aContentXML;
 - (void)tag:(NSString *)aTagName attributes:(NSDictionary *)anAttributeDictionary contentText:(NSString *)aContentText;
 - (void)tag:(NSString *)aTagName attributes:(NSDictionary *)anAttributeDictionary contentCDATA:(NSString *)aContentCDATA;
+
+// shortcuts for attributeless tags
+- (void)tag:(NSString *)aTagName contentBlock:(void (^)(void))aContentBlock;
+- (void)tag:(NSString *)aTagName contentXML:(NSString *)aContentXML;
+- (void)tag:(NSString *)aTagName contentText:(NSString *)aContentText;
+- (void)tag:(NSString *)aTagName contentCDATA:(NSString *)aContentCDATA;
+
 
 // content
 - (void)text:(NSString *)aTextString;
